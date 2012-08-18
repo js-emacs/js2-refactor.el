4cbe02ff066b4e7fbb1cec9f59031ec633a8a02a
Magnar Sveen <magnars@gmail.com>
2012-08-18 13:19:29 +0200

    wrap and unwrap no longer gets confused about extra whitespace

--- js2r-wrapping.el
+++ js2r-wrapping.el
@@ -1,23 +1,43 @@
-(defun js2r-unwrap (beg end)
-  (interactive "r")
+(defvar js2r--space-str " \t\n")
+
+(defun js2r--skip-region-whitespace ()
+  (let ((p-first (< (point) (mark))))
+    (unless p-first
+      (exchange-point-and-mark))
+    (skip-chars-forward js2r--space-str)
+    (exchange-point-and-mark)
+    (skip-chars-backward js2r--space-str)
+    (when p-first
+      (exchange-point-and-mark))))
+
+(defun js2r-unwrap ()
+  (interactive)
   (js2r--guard)
-  (unless (use-region-p)
-    (let ((stmt (js2-node-parent-stmt (js2-node-at-point))))
-      (setq beg (js2-node-abs-pos stmt))
-      (setq end (js2-node-abs-end stmt))))
-  (let* ((ancestor (js2-node-parent-stmt
-                    (js2r--first-common-ancestor-in-region beg end)))
-         (abeg (js2-node-abs-pos ancestor))
-         (aend (js2-node-abs-end ancestor)))
-    (save-excursion
-      (goto-char end)
-      (delete-char (- aend end))
-      (goto-char abeg)
-      (delete-char (- beg abeg)))
-    (indent-region (point-min) (point-max))))
+  (let (beg end)
+    (if (use-region-p)
+        (progn
+          (js2r--skip-region-whitespace)
+          (setq beg (min (point) (mark)))
+          (setq end (max (point) (mark))))
+      (let ((stmt (js2-node-parent-stmt (js2-node-at-point))))
+        (setq beg (js2-node-abs-pos stmt))
+        (setq end (js2-node-abs-end stmt))))
+    (let* ((ancestor (js2-node-parent-stmt
+                      (js2r--first-common-ancestor-in-region beg end)))
+           (abeg (js2-node-abs-pos ancestor))
+           (aend (js2-node-abs-end ancestor)))
+      (save-excursion
+        (goto-char end)
+        (delete-char (- aend end))
+        (goto-char abeg)
+        (delete-char (- beg abeg)))
+      (indent-region (point-min) (point-max)))))
 
 (defun js2r-wrap-in-for-loop (beg end)
   (interactive "r")
+  (js2r--skip-region-whitespace)
+  (setq beg (min (point) (mark)))
+  (setq end (max (point) (mark)))
   (let ((s (buffer-substring beg end)))
     (yas/expand-snippet (concat
                          "for (var i = 0, l = ${1:length}; i < l; i++) {\n" s "$0\n}")
