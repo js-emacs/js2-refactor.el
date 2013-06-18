325a59d15d5e697a57fec0ac176c48b3588cb4a9
Alex Chamberlain <apchamberlain@gmail.com>
2013-06-18 07:58:06 -0700

    First try at expand/contract for functions and arrays

--- js2r-formatting.el
+++ js2r-formatting.el
@@ -25,29 +25,106 @@
     (just-one-space))
   (just-one-space))
 
-(defmacro js2r--create-object-whitespace-traverser (name func)
+(defmacro js2r--create-bracketed-whitespace-traverser
+  (name ws-fix-func looking-at-start-func
+	goto-closest-start-func subexpr-str)
+  "Build a function to expand or contract a given type of
+   bracketed expression, i.e., function body, object literal, or
+   array.
+   Parameters:
+       name:                    name of the function to be built
+       ws-fix-func:             function to adjust whitespace at point
+       looking-at-start-func:   returns t if point is at
+                                    the start of the bracketed
+                                    thing we want to act on
+       goto-closest-start-func: moves point if necessary
+                                    until looking-at-start-func
+                                    is true
+       subexpr-str:             literal delimiter of parts of the
+                                    thing to be expanded or contracted"
   `(defun ,name ()
      (interactive)
      (save-excursion
-       (if (not (js2r--looking-at-object-start))
-           (js2r--goto-closest-object-start))
+       (if (not ,looking-at-start-func)
+           ,goto-closest-start-func)
        (let ((end (make-marker)))
          (set-marker end (save-excursion
                            (forward-list)
                            (point)))
          (forward-char)
-         ,func
+         ,ws-fix-func
          (while (< (point) end)
            (while (js2r--point-inside-string-p)
              (forward-char))
-           (when (looking-at ",")
+           (when (looking-at ,subexpr-str)
              (forward-char)
-             ,func)
-           (if (looking-at "\\s(")
+             ,ws-fix-func)
+           (if (looking-at "\\s(")  ; FIXME: This might be more elegant if
+				    ; abstracted out into a "looking-at-list-start"
+				    ; function.
                (forward-list)
              (forward-char)))
          (backward-char)
-         ,func))))
+         ,ws-fix-func))))
+
+(js2r--create-bracketed-whitespace-traverser js2r-expand-object
+					     (js2r--ensure-newline)
+					     (js2r--looking-at-object-start)
+					     (js2r--goto-closest-object-start)
+					     ",")
+
+(js2r--create-bracketed-whitespace-traverser js2r-contract-object
+					     (js2r--ensure-just-one-space)
+					     (js2r--looking-at-object-start)
+					     (js2r--goto-closest-object-start)
+					     ",")
+
+(defun js2r--looking-at-array-start ()
+  (looking-at "\\["))
+
+(defun js2r--goto-closest-array-start ()
+  (while (not (js2r--looking-at-array-start))
+    (if (eq (car (syntax-ppss)) 0)
+        (error "Cursor is not on an array")
+      (goto-char (nth 1 (syntax-ppss))))))
+
+(js2r--create-bracketed-whitespace-traverser js2r-expand-array
+					     (js2r--ensure-newline)
+					     (js2r--looking-at-array-start)
+					     (js2r--goto-closest-array-start)
+					     ",")
+
+(js2r--create-bracketed-whitespace-traverser js2r-contract-array
+					     (js2r--ensure-just-one-space)
+					     (js2r--looking-at-array-start)
+					     (js2r--goto-closest-array-start)
+					     ",")
+(defun js2r--looking-at-function-start ()
+  (and (looking-at "{")
+       ;; FIXME: Look backward for the "function" keyword, separated
+       ;; from the open paren by, at most, a valid identifier and
+       ;; whitespace.  Right now this will try to expand anything
+       ;; surrounded by braces, e.g., a "for" loop body.
+       (looking-back ")[\s\n]*")))
+
+(defun js2r--goto-closest-function-start ()
+  (while (not (js2r--looking-at-function-start))
+    (if (eq (car (syntax-ppss)) 0)
+        (error "Cursor is not on a function body")
+      (goto-char (nth 1 (syntax-ppss))))))
+
+(js2r--create-bracketed-whitespace-traverser js2r-expand-function
+					     (js2r--ensure-newline)
+					     (js2r--looking-at-function-start)
+					     (js2r--goto-closest-function-start)
+					     ";")
+
+(js2r--create-bracketed-whitespace-traverser js2r-contract-function
+					     (js2r--ensure-just-one-space)
+					     (js2r--looking-at-function-start)
+					     (js2r--goto-closest-function-start)
+					     ";")
+
 
 (js2r--create-object-whitespace-traverser js2r-expand-object
                                         (js2r--ensure-newline))
