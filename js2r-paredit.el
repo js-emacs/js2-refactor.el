577deac65149dbfdbccbda32f382eff2b565d7d1
Nicolas Petton <nicolas@petton.fr>
2015-01-22 20:33:35 +0100

    Add comments to js2r-helpers.el and js2r-paredit.el

--- js2r-paredit.el
+++ js2r-paredit.el
@@ -1,3 +1,25 @@
+;;; js2r-paredit.el --- Paredit-like extensions for js2-refactor
+
+;; Copyright (C) 2012-2015 Magnar Sveen
+
+;; Author: Magnar Sveen <magnars@gmail.com>
+;; Keywords: conveniences
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Code:
+
 (require 'dash)
 
 (defun js2r--nesting-node-p (node)
@@ -153,5 +175,4 @@ When at the beginning of the node, kill from outside of it."
         (list :beg standalone-end :end standalone-end :contents text))))))
 
 (provide 'js2r-paredit)
-
 ;;; js2r-paredit.el ends here
