b3b57463b11c0105fcdd6846eef17793ce88e29b
Nicolas Petton <nicolas@petton.fr>
2016-09-15 14:23:44 +0200

    Fix test step definitions to work with Emacs-25

--- features/step-definitions/js2-refactor-steps.el
+++ features/step-definitions/js2-refactor-steps.el
@@ -1,3 +1,5 @@
+(require 'cl-lib)
+
 (And "^I turn on js2-mode and js2-refactor-mode$"
      (lambda ()
        (js2-mode)
@@ -13,12 +15,12 @@
         (goto-char (point-min))
         (let ((search (re-search-forward (format "%s" char) nil t))
               (message "Can not go to character '%s' since it does not exist in the current buffer: %s"))
-          (assert search nil message char (espuds-buffer-contents)))))
+          (cl-assert search nil message char (espuds-buffer-contents)))))
 
 (When "^I go to the \\(front\\|end\\) of the word \"\\(.+\\)\"$"
       (lambda (pos word)
         (goto-char (point-min))
         (let ((search (re-search-forward (format "%s" word) nil t))
               (message "Can not go to character '%s' since it does not exist in the current buffer: %s"))
-          (assert search nil message word (espuds-buffer-contents))
+          (cl-assert search nil message word (espuds-buffer-contents))
           (if (string-equal "front" pos) (backward-word)))))
